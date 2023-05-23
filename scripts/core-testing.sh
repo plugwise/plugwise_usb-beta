63b24283fc05f46d8d235e5304b8ee129e29340f
Tom Scholten <git@scholten.nu>
2023-05-23 08:40:15 +0200

    Fix ruff

--- scripts/core-testing.sh
+++ scripts/core-testing.sh
@@ -17,7 +17,7 @@ set -e
 # Which packages to install (to prevent installing all test requirements)
 # actual package version ARE verified (i.e. grepped) from requirements_test_all
 # separate packages with |
-pip_packages="fnvhash|lru-dict|voluptuous|pyroute2|sqlalchemy|zeroconf|pyserial|pytest-socket|pre-commit|paho-mqtt|numpy|pydantic"
+pip_packages="fnvhash|lru-dict|voluptuous|pyroute2|sqlalchemy|zeroconf|pyserial|pytest-socket|pre-commit|paho-mqtt|numpy|pydantic|ruff"
 
 echo ""
 echo "Checking for necessary tools and preparing setup:"
@@ -184,8 +184,6 @@ if [ -z "${GITHUB_ACTIONS}" ] || [ "$1" == "pip_prep" ] ; then
 	grep -hEi "${pip_packages}" requirements_test_all.txt > ./tmp/requirements_test_extra.txt
 	echo " - extra's required for plugwise_usb"
 	pip install --upgrade -q --disable-pip-version-check -r ./tmp/requirements_test_extra.txt
-	echo " - ruff"
-	pip install --upgrade -q ruff
 	echo ""
         # When using test.py prettier makes multi-line, so use jq
 	#module=$(grep require ../custom_components/plugwise_usb/manifest.json | cut -f 4 -d '"')
