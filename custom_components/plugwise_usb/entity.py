2dc16e799bab7af060b86d25ed518bcf76d3dc00
Breugel <frank_van_breugel@hotmail.com>
2024-01-18 20:43:03 +0100

    First initial untested rewrite for async version of backend lib.

---
+++ custom_components/plugwise_usb/entity.py
@@ -0,0 +1,53 @@
+"""Plugwise USB stick base entity."""
+
+from dataclasses import dataclass
+
+from homeassistant.helpers.entity import EntityDescription
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+)
+
+from .plugwise_usb.api import (
+    NodeFeature,
+    NodeInfo,
+)
+
+from .const import DOMAIN
+from .coordinator import PlugwiseUSBDataUpdateCoordinator
+
+
+@dataclass
+class PlugwiseUSBRequiredKeysMixin:
+    """Default mixin for required keys."""
+
+    feature: NodeFeature
+
+
+@dataclass
+class PlugwiseUSBEntityDescription(EntityDescription, PlugwiseUSBRequiredKeysMixin):
+    """Describes Toon sensor entity."""
+
+
+class PlugwiseUSBEntity(CoordinatorEntity):
+    """Representation of a base class for Plugwise USB entity."""
+
+    entity_description: PlugwiseUSBEntityDescription
+
+    def __init__(
+        self,
+        coordinator: PlugwiseUSBDataUpdateCoordinator,
+        entity_description: PlugwiseUSBEntityDescription,
+    ) -> None:
+        """Initialize a Plugwise USB entity."""
+        super().__init__(coordinator, context=entity_description.feature)
+        node_info: NodeInfo = self.coordinator.data[NodeFeature.INFO]
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, node_info.mac)},
+            "name": node_info.name,
+            "manufacturer": "Plugwise",
+            "model": node_info.model,
+            "sw_version": f"{node_info.firmware}",
+        }
+        self._attr_name = entity_description.name
+        self._attr_unique_id = f"{node_info.mac}-{entity_description.key}"
+        self.entity_description = entity_description
