a8f5cb2945513aec68474851f78cca0172001643
Breugel <frank_van_breugel@hotmail.com>
2024-02-06 22:03:15 +0100

    Rewrite
    - Loading of integration does not wait for loading nodes
    - Added relay control
    - Add support for motion (scan)
    - Utilize the entity translation names

--- custom_components/plugwise_usb/__init__.py
+++ custom_components/plugwise_usb/__init__.py
@@ -16,13 +16,14 @@ from .const import (
 )
 from .coordinator import PlugwiseUSBDataUpdateCoordinator
 from .plugwise_usb import Stick
+from .plugwise_usb.api import NodeEvent
 from .plugwise_usb.exceptions import StickError
 
 _LOGGER = logging.getLogger(__name__)
-
+UNSUBSCRIBE_DISCOVERY = "unsubscribe_discovery"
 
 class NodeConfigEntry(TypedDict):
-    """Plugwise node config entry"""
+    """Plugwise node config entry."""
 
     mac: str
     coordinator: PlugwiseUSBDataUpdateCoordinator
@@ -38,50 +39,60 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     )
     hass.data[DOMAIN][config_entry.entry_id] = {STICK: api_stick}
 
-    _LOGGER.debug("Connect to Plugwise USB-Stick")
+    _LOGGER.debug("Connect & initialize Plugwise USB-Stick")
     try:
         await api_stick.connect()
+        await api_stick.initialize()
     except StickError:
         raise ConfigEntryNotReady(
             f"Failed to open connection to Plugwise USB stick at {config_entry.data[CONF_USB_PATH]}"
         ) from StickError
 
-    _LOGGER.debug("Initialize Plugwise USB-Stick")
-    try:
-        await api_stick.initialize()
-    except StickError:
-        await api_stick.disconnect()
-        raise ConfigEntryNotReady(
-            "Failed to initialize connection to Plugwise USB-Stick"
-        ) from StickError
-
-    _LOGGER.debug("Discover registered Plugwise nodes")
-    try:
-        await api_stick.discover_nodes(load=False)
-    except StickError:
-        await api_stick.disconnect()
-        raise ConfigEntryNotReady(
-            "Failed to discover Plugwise USB nodes"
-        ) from StickError
-
     hass.data[DOMAIN][config_entry.entry_id][NODES]: NodeConfigEntry = {}
 
-    # Setup a 'Data Update Coordinator' for each plugwise node
-    for mac, node in api_stick.nodes.items():
-        _LOGGER.debug("Try to load Plugwise USB-Stick node %s", mac)
-        if await node.load():
-            _LOGGER.debug("Try to load Plugwise USB-Stick node %s", mac)
-            coordinator = PlugwiseUSBDataUpdateCoordinator(hass, node)
-            await coordinator.async_config_entry_first_refresh()
-            hass.data[DOMAIN][config_entry.entry_id][NODES][mac] = coordinator
+    async def async_node_discovered(node_event: NodeEvent, mac: str) -> None:
+        """Node is detected."""
+        if node_event != NodeEvent.DISCOVERED:
+            return
+        _LOGGER.debug("async_node_discovered | mac=%s", mac)
+        node = api_stick.nodes[mac]
+        _LOGGER.debug("async_node_discovered | node_info=%s", node.node_info)
+        coordinator = PlugwiseUSBDataUpdateCoordinator(hass, node)
+        hass.data[DOMAIN][config_entry.entry_id][NODES][mac] = coordinator
+        await coordinator.async_config_entry_first_refresh()
+        await node.load()
+
+    hass.data[DOMAIN][config_entry.entry_id][UNSUBSCRIBE_DISCOVERY] = (
+        api_stick.subscribe_to_node_events(
+            async_node_discovered,
+            (NodeEvent.DISCOVERED,),
+        )
+    )
+
+    _LOGGER.debug("Discover network coordinator")
+    try:
+        await api_stick.discover_coordinator(load=False)
+    except StickError:
+        await api_stick.disconnect()
+        raise ConfigEntryNotReady(
+            "Failed to connect to Circle+"
+        ) from StickError
+
+    # Load platforms to allow them to register for node events
     await hass.config_entries.async_forward_entry_setups(
         config_entry, PLUGWISE_USB_PLATFORMS
+    )
+
+    # Initiate background discovery task
+    hass.async_create_task(
+        api_stick.discover_nodes(load=True)
     )
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Unload the Plugwise USB stick connection."""
+    hass.data[DOMAIN][config_entry.entry_id][UNSUBSCRIBE_DISCOVERY]()
     unload = await hass.config_entries.async_unload_platforms(
         config_entry, PLUGWISE_USB_PLATFORMS
     )
--- custom_components/plugwise_usb/entity.py
+++ custom_components/plugwise_usb/entity.py
@@ -1,6 +1,9 @@
 """Plugwise USB stick base entity."""
+from __future__ import annotations
 
+from collections.abc import Callable
 from dataclasses import dataclass
+import logging
 
 from homeassistant.helpers.entity import EntityDescription
 from homeassistant.helpers.update_coordinator import (
@@ -9,45 +12,72 @@ from homeassistant.helpers.update_coordinator import (
 
 from .const import DOMAIN
 from .coordinator import PlugwiseUSBDataUpdateCoordinator
-from .plugwise_usb.api import (
-    NodeFeature,
-    NodeInfo,
-)
+
+from .plugwise_usb.api import NodeFeature, NodeInfo, PUSHING_FEATURES
+
+_LOGGER = logging.getLogger(__name__)
 
 
-@dataclass
-class PlugwiseUSBRequiredKeysMixin:
-    """Default mixin for required keys."""
+@dataclass(kw_only=True)
+class PlugwiseUSBEntityDescription(EntityDescription):
+    """Describes Plugwise sensor entity."""
 
-    feature: NodeFeature
-
-
-@dataclass
-class PlugwiseUSBEntityDescription(EntityDescription, PlugwiseUSBRequiredKeysMixin):
-    """Describes Toon sensor entity."""
+    node_feature: NodeFeature
 
 
 class PlugwiseUSBEntity(CoordinatorEntity):
     """Representation of a base class for Plugwise USB entity."""
 
-    entity_description: PlugwiseUSBEntityDescription
+    _attr_has_entity_name = True
 
     def __init__(
         self,
-        coordinator: PlugwiseUSBDataUpdateCoordinator,
+        node_duc: PlugwiseUSBDataUpdateCoordinator,
         entity_description: PlugwiseUSBEntityDescription,
     ) -> None:
         """Initialize a Plugwise USB entity."""
-        super().__init__(coordinator, context=entity_description.feature)
-        node_info: NodeInfo = self.coordinator.data[NodeFeature.INFO]
+        self.node_duc = node_duc
+        super().__init__(node_duc, context=entity_description.node_feature)
+        self._node_info: NodeInfo = self.coordinator.data[NodeFeature.INFO]
         self._attr_device_info = {
-            "identifiers": {(DOMAIN, node_info.mac)},
-            "name": node_info.name,
+            "identifiers": {(DOMAIN, self._node_info.mac)},
+            "name": f"{self._node_info.model} {self._node_info.name}",
             "manufacturer": "Plugwise",
-            "model": node_info.model,
-            "sw_version": f"{node_info.firmware}",
+            "model": self._node_info.model,
+            "sw_version": f"{self._node_info.firmware}",
         }
-        self._attr_name = f"{entity_description.name} ({node_info.mac[-5:]})"
-        self._attr_unique_id = f"{node_info.mac}-{entity_description.key}"
-        self._attr_should_poll = False
+        self._attr_unique_id = f"{self._node_info.mac}-{entity_description.key}"
         self.entity_description = entity_description
+        self._subscribe_to_feature_fn = node_duc.node.subscribe_to_feature_update
+        self.unsubscribe_push_events: Callable[[], None] | None = None
+
+    async def async_added_to_hass(self):
+        """Subscribe for push updates."""
+        await super().async_added_to_hass()
+        push_features = tuple(
+            push_feature for push_feature in PUSHING_FEATURES
+            if push_feature in self._node_info.features
+        )
+        # Subscribe to events
+        if push_features:
+            self.unsubscribe_push_events = self._subscribe_to_feature_fn(
+                self.async_push_event,
+                push_features,
+            )
+
+    async def async_push_event(self, feature: NodeFeature, state: Any) -> None:
+        """"Update data on pushed event."""
+        if self.node_duc is None:
+            _LOGGER.warning("Unable to push event=%s, state=%s, mac=%s", feature, state, self._node_info.mac)
+        await self.node_duc.async_set_updated_data(
+            {
+                feature: state,
+            }
+        )
+
+    async def async_will_remove_from_hass(self):
+        """Unsubscribe to updates."""
+        if self.unsubscribe_push_events is not None:
+            self.unsubscribe_push_events()
+            self.unsubscribe_push_events = None
+        await super().async_will_remove_from_hass()
