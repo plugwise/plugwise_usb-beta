ef4445afbe6551cfb241921f18bd30c4d1f05102
Breugel <frank_van_breugel@hotmail.com>
2024-02-13 20:54:27 +0100

    Correct imports for async backend lib

--- custom_components/plugwise_usb/binary_sensor.py
+++ custom_components/plugwise_usb/binary_sensor.py
@@ -14,11 +14,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from plugwise_usb.api import NodeEvent, NodeFeature
 
 from .const import DOMAIN, NODES, STICK, UNSUB_NODE_LOADED
 from .entity import PlugwiseUSBEntity, PlugwiseUSBEntityDescription
-
-from .plugwise_usb.api import NodeEvent, NodeFeature, NodeFeature
 
 _LOGGER = logging.getLogger(__name__)
 PARALLEL_UPDATES = 2
--- custom_components/plugwise_usb/sensor.py
+++ custom_components/plugwise_usb/sensor.py
@@ -13,10 +13,10 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
-    Platform,
+    PERCENTAGE,
     SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
     EntityCategory,
-    PERCENTAGE,
+    Platform,
     UnitOfEnergy,
     UnitOfPower,
     UnitOfTemperature,
@@ -24,11 +24,10 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from plugwise_usb.api import NodeEvent, NodeFeature
 
 from .const import DOMAIN, NODES, STICK, UNSUB_NODE_LOADED
 from .entity import PlugwiseUSBEntity, PlugwiseUSBEntityDescription
-
-from .plugwise_usb.api import NodeEvent, NodeFeature
 
 _LOGGER = logging.getLogger(__name__)
 PARALLEL_UPDATES = 2
