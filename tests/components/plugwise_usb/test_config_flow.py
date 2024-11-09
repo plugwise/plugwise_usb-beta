52c691d985b786739869b7ef402f27ba71fde873
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2024-11-09 13:52:34 +0100

    More test-improvements

--- tests/components/plugwise_usb/test_config_flow.py
+++ tests/components/plugwise_usb/test_config_flow.py
@@ -26,27 +26,6 @@ def com_port():
     return port
 
 
-async def test_form_flow_usb(
-    hass: HomeAssistant,
-    mock_setup_entry: AsyncMock,
-) -> None:
-    """Test we get the form for Plugwise USB product type."""
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN, context={CONF_SOURCE: SOURCE_USER}
-    )
-    assert result.get("type") is FlowResultType.FORM
-    assert result.get("errors") == {}
-    assert result.get("step_id") == "user"
-    assert "flow_id" in result
-
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"],
-    )
-    assert result.get("type") is FlowResultType.FORM
-    assert result.get("errors") == {}
-    assert result.get("step_id") == "user"
-
-
 @patch("serial.tools.list_ports.comports", MagicMock(return_value=[com_port()]))
 async def test_user_flow_select(hass):
     """Test user flow when USB-stick is selected from list."""
@@ -57,6 +36,10 @@ async def test_user_flow_select(hass):
         DOMAIN,
         context={CONF_SOURCE: SOURCE_USER},
     )
+    assert result.get("type") is FlowResultType.FORM
+    assert result.get("errors") == {}
+    assert result.get("step_id") == "user"
+    assert "flow_id" in result
 
     with patch(
         "homeassistant.components.plugwise_usb.config_flow.Stick",
@@ -66,23 +49,24 @@ async def test_user_flow_select(hass):
         usb_mock.return_value.disconnect = AsyncMock(return_value=None)
         usb_mock.return_value.mac_stick = MagicMock(return_value="01:23:45:67:AB")
     
-        result_2 = await hass.config_entries.flow.async_configure(
+        result = await hass.config_entries.flow.async_configure(
             result["flow_id"], user_input={CONF_USB_PATH: port_select}
         )
         await hass.async_block_till_done()
-        assert result_2.get("type") is FlowResultType.CREATE_ENTRY
-        assert result_2.get("data") == {CONF_USB_PATH: TEST_USBPORT}
+        assert result.get("type") is FlowResultType.CREATE_ENTRY
+        assert result.get("data") == {CONF_USB_PATH: TEST_USBPORT}
 
-    # Retry to ensure configuring the same port is not allowed
-    result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={CONF_SOURCE: SOURCE_USER},
-    )
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], user_input={CONF_USB_PATH: port_select}
-    )
-    assert result.get("type") is FlowResultType.FORM
-    assert result.get("errors") == {"base": "already_configured"}
+        # Retry to ensure configuring the same port is not allowed
+        result = await hass.config_entries.flow.async_init(
+            DOMAIN,
+            context={CONF_SOURCE: SOURCE_USER},
+        )
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input={CONF_USB_PATH: port_select}
+        )
+        await hass.async_block_till_done()
+        assert result.get("type") is FlowResultType.FORM
+        assert result.get("errors") == {"base": "already_configured"}
 
 
 async def test_user_flow_manual_selected_show_form(hass):
@@ -105,7 +89,7 @@ async def test_user_flow_manual(hass):
         DOMAIN,
         context={CONF_SOURCE: SOURCE_USER},
     )
-    result_2 = await hass.config_entries.flow.async_configure(
+    result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
         user_input={CONF_USB_PATH: CONF_MANUAL_PATH},
     )
@@ -118,12 +102,13 @@ async def test_user_flow_manual(hass):
         usb_mock.return_value.disconnect = AsyncMock(return_value=None)
         usb_mock.return_value.mac_stick = MagicMock(return_value="01:23:45:67:AB")
 
-        result_3 = await hass.config_entries.flow.async_configure(
-            result_2["flow_id"],
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
             user_input={CONF_USB_PATH: TEST_USBPORT2},
         )
-    assert result_3.get("type") is FlowResultType.CREATE_ENTRY
-    assert result_3.get("data") == {CONF_USB_PATH: TEST_USBPORT2}
+        await hass.async_block_till_done()
+        assert result.get("type") is FlowResultType.CREATE_ENTRY
+        assert result.get("data") == {CONF_USB_PATH: TEST_USBPORT2}
 
 
 async def test_invalid_connection(hass):
@@ -136,12 +121,13 @@ async def test_invalid_connection(hass):
         result["flow_id"],
         user_input={CONF_USB_PATH: CONF_MANUAL_PATH},
     )
+    await hass.async_block_till_done()
 
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
         {CONF_USB_PATH: "/dev/null"},
     )
-
+    await hass.async_block_till_done()
     assert result.get("type") is FlowResultType.FORM
     assert result.get("errors") == {"base": "cannot_connect"}
 
@@ -156,6 +142,7 @@ async def test_empty_connection(hass):
         result["flow_id"],
         user_input={CONF_USB_PATH: CONF_MANUAL_PATH},
     )
+    await hass.async_block_till_done()
 
     try:
         result = await hass.config_entries.flow.async_configure(
@@ -182,10 +169,12 @@ async def test_failed_connect(hass):
         result["flow_id"],
         user_input={CONF_USB_PATH: CONF_MANUAL_PATH},
     )
+    await hass.async_block_till_done()
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
         user_input={CONF_USB_PATH: "/dev/null"},
     )
+    await hass.async_block_till_done()
     assert result.get("type") is FlowResultType.FORM
     assert result.get("errors") == {"base": "cannot_connect"}
 
@@ -202,9 +191,11 @@ async def test_failed_initialization(hass):
         result["flow_id"],
         user_input={CONF_USB_PATH: CONF_MANUAL_PATH},
     )
+    await hass.async_block_till_done()
     result = await hass.config_entries.flow.async_configure(
         result["flow_id"],
         user_input={CONF_USB_PATH: "/dev/null"},
     )
+    await hass.async_block_till_done()
     assert result.get("type") is FlowResultType.FORM
     assert result.get("errors") == {"base": "stick_init"}
