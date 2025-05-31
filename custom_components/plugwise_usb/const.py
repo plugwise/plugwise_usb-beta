cfda7be9bf9d263d41c6d96359de043fb4ccd28e
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2025-05-31 16:21:41 +0200

    Implement constants review suggestions

--- custom_components/plugwise_usb/const.py
+++ custom_components/plugwise_usb/const.py
@@ -8,18 +8,18 @@ import voluptuous as vol
 from homeassistant.const import Platform
 from homeassistant.helpers import config_validation as cv
 
-DOMAIN: Final = "plugwise_usb"
+DOMAIN: Final[str] = "plugwise_usb"
 
 LOGGER = logging.getLogger(__package__)
-UNSUB_NODE_LOADED: Final = "Unsubcribe_from_node_loaded_event"
-COORDINATOR: Final = "coordinator"
-CONF_MANUAL_PATH: Final = "Enter Manually"
-MANUAL_PATH: Final = "manual_path"
-STICK: Final = "stick"
-NODES: Final = "nodes"
-USB: Final = "usb"
+UNSUB_NODE_LOADED: Final[str] = "Unsubcribe_from_node_loaded_event"
+COORDINATOR: Final[str] = "coordinator"
+CONF_MANUAL_PATH: Final[str] = "Enter Manually"
+MANUAL_PATH: Final[str] = "manual_path"
+STICK: Final[str] = "stick"
+NODES: Final[str] = "nodes"
+USB: Final[str] = "usb"
 
-UNDO_UPDATE_LISTENER: Final = "undo_update_listener"
+UNDO_UPDATE_LISTENER: Final[str] = "undo_update_listener"
 
 PLUGWISE_USB_PLATFORMS: Final[list[str]] = [
     Platform.BINARY_SENSOR,
@@ -27,13 +27,11 @@ PLUGWISE_USB_PLATFORMS: Final[list[str]] = [
     Platform.SENSOR,
     Platform.SWITCH,
 ]
-CONF_USB_PATH: Final = "usb_path"
-
-ATTR_MAC_ADDRESS: Final = "mac"
-
-SERVICE_AUTO_JOIN: Final = "enable_auto_joining"
-SERVICE_DISABLE_PRODUCTION: Final = "disable_production"
-SERVICE_ENABLE_PRODUCTION: Final = "enable_production"
+CONF_USB_PATH: Final[str] = "usb_path"
+ATTR_MAC_ADDRESS: Final[str] = "mac"
+SERVICE_AUTO_JOIN: Final[str] = "enable_auto_joining"
+SERVICE_DISABLE_PRODUCTION: Final[str] = "disable_production"
+SERVICE_ENABLE_PRODUCTION: Final[str] = "enable_production"
 SERVICE_USB_DEVICE_SCHEMA: Final = vol.Schema(
     {
         vol.Required(ATTR_MAC_ADDRESS): vol.All(
@@ -44,13 +42,13 @@ SERVICE_USB_DEVICE_SCHEMA: Final = vol.Schema(
 )
 
 # USB SED (battery powered) device constants
-ATTR_SED_STAY_ACTIVE: Final = "stay_active"
-ATTR_SED_SLEEP_FOR: Final = "sleep_for"
-ATTR_SED_MAINTENANCE_INTERVAL: Final = "maintenance_interval"
-ATTR_SED_CLOCK_SYNC: Final = "clock_sync"
-ATTR_SED_CLOCK_INTERVAL: Final = "clock_interval"
+ATTR_SED_STAY_ACTIVE: Final[str] = "stay_active"
+ATTR_SED_SLEEP_FOR: Final[str] = "sleep_for"
+ATTR_SED_MAINTENANCE_INTERVAL: Final[str] = "maintenance_interval"
+ATTR_SED_CLOCK_SYNC: Final[str] = "clock_sync"
+ATTR_SED_CLOCK_INTERVAL: Final[str] = "clock_interval"
 
-SERVICE_USB_SED_BATTERY_CONFIG: Final = "configure_battery_savings"
+SERVICE_USB_SED_BATTERY_CONFIG: Final[str] = "configure_battery_savings"
 SERVICE_USB_SED_BATTERY_CONFIG_SCHEMA: Final = {
     vol.Required(ATTR_SED_STAY_ACTIVE): vol.All(
         vol.Coerce(int), vol.Range(min=1, max=120)
@@ -69,20 +67,20 @@ SERVICE_USB_SED_BATTERY_CONFIG_SCHEMA: Final = {
 
 
 # USB Scan device constants
-ATTR_SCAN_DAYLIGHT_MODE: Final = "day_light"
-ATTR_SCAN_SENSITIVITY_MODE: Final = "sensitivity_mode"
-ATTR_SCAN_RESET_TIMER: Final = "reset_timer"
+ATTR_SCAN_DAYLIGHT_MODE: Final[str] = "day_light"
+ATTR_SCAN_SENSITIVITY_MODE: Final[str] = "sensitivity_mode"
+ATTR_SCAN_RESET_TIMER: Final[str] = "reset_timer"
 
-SCAN_SENSITIVITY_HIGH: Final = "high"
-SCAN_SENSITIVITY_MEDIUM: Final = "medium"
-SCAN_SENSITIVITY_OFF: Final = "off"
+SCAN_SENSITIVITY_HIGH: Final[str] = "high"
+SCAN_SENSITIVITY_MEDIUM: Final[str] = "medium"
+SCAN_SENSITIVITY_OFF: Final[str] = "off"
 SCAN_SENSITIVITY_MODES = [
     SCAN_SENSITIVITY_HIGH,
     SCAN_SENSITIVITY_MEDIUM,
     SCAN_SENSITIVITY_OFF,
 ]
 
-SERVICE_USB_SCAN_CONFIG: Final = "configure_scan"
+SERVICE_USB_SCAN_CONFIG: Final[str] = "configure_scan"
 SERVICE_USB_SCAN_CONFIG_SCHEMA = (
     {
         vol.Required(ATTR_SCAN_SENSITIVITY_MODE): vol.In(SCAN_SENSITIVITY_MODES),
