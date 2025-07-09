1614b835af099c505959cd6eb465c80c61d0ed0d
Marc Dirix <marc@dirix.nu>
2025-07-09 22:06:51 +0200

    doh

--- custom_components/plugwise_usb/coordinator.py
+++ custom_components/plugwise_usb/coordinator.py
@@ -116,7 +116,7 @@ class PlugwiseUSBDataUpdateCoordinator(DataUpdateCoordinator):
     async def unsubscribe_all_nodefeatures(self) -> None:
         """Unsubscribe to updates."""
         for unsubscribe_push_event in self.unsubscribe_push_events:
-            self.unsubscribe_push_events.pop(self.unsubscribe_push_event)
-            self.unsubscribe_push_event()
+            self.unsubscribe_push_events.pop(unsubscribe_push_event)
+            unsubscribe_push_event()
         self.subscribed_nodefeatures = []
 
