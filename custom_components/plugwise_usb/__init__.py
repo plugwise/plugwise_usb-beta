dbe8c29d743f2ea621a75e267e155817eb16d3eb
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-02-07 15:08:23 +0100

    Remove serial_number

--- custom_components/plugwise_usb/__init__.py
+++ custom_components/plugwise_usb/__init__.py
@@ -75,7 +75,6 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: PlugwiseUSBConfig
         manufacturer="Plugwise",
         model="Stick",
         name=str(api_stick.name),
-        serial_number=str(api_stick.mac_stick),
         sw_version=str(api_stick.firmware),
     )
 
