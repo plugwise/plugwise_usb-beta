eb477e0964ed54aeaa253689ceda716e2f135318
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-02-25 19:53:39 +0100

    Improve async_step_reconfigure()

--- custom_components/plugwise_usb/config_flow.py
+++ custom_components/plugwise_usb/config_flow.py
@@ -112,7 +112,9 @@ class PlugwiseUSBConfigFlow(ConfigFlow, domain=DOMAIN):
             )
             errors, mac_stick = await validate_usb_connection(self.hass, device_path)
             if not errors:
-                await self.async_set_unique_id(mac_stick)
+                await self.async_set_unique_id(
+                    unique_id=mac_stick, raise_on_progress=False
+                )
                 return self.async_create_entry(
                     title="Stick", data={CONF_USB_PATH: device_path}
                 )
@@ -131,40 +133,34 @@ class PlugwiseUSBConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         reconfigure_entry = self._get_reconfigure_entry()
 
-        ports = await self.hass.async_add_executor_job(serial.tools.list_ports.comports)
-        list_of_ports = [
-            f"{p}, s/n: {p.serial_number or 'n/a'}"
-            + (f" - {p.manufacturer}" if p.manufacturer else "")
-            for p in ports
-        ]
-        list_of_ports.append(CONF_MANUAL_PATH)
+        # ports = await self.hass.async_add_executor_job(serial.tools.list_ports.comports)
+        # list_of_ports = [
+        #     f"{p}, s/n: {p.serial_number or 'n/a'}"
+        #     + (f" - {p.manufacturer}" if p.manufacturer else "")
+        #     for p in ports
+        # ]
+        # list_of_ports.append(CONF_MANUAL_PATH)
 
         if user_input is not None:
-            user_selection = user_input[CONF_USB_PATH]
-
-            if user_selection == CONF_MANUAL_PATH:
-                return await self.async_step_manual_path()
-
-            port = ports[list_of_ports.index(user_selection)]
-            device_path = await self.hass.async_add_executor_job(
-                usb.get_serial_by_id, port.device
-            )
-            errors, mac_stick = await validate_usb_connection(self.hass, device_path)
+            full_input = user_input.get(CONF_USB_PATH)
+            # user_selection = user_input[CONF_USB_PATH]
+            # port = ports[list_of_ports.index(user_selection)]
+            # device_path = await self.hass.async_add_executor_job(
+            #     usb.get_serial_by_id, port.device
+            # )
+            errors, mac_stick = await validate_usb_connection(self.hass, full_input)
             if not errors:
-                await self.async_set_unique_id(mac_stick)
+                await self.async_set_unique_id(mac_stick, raise_on_progress=False)
                 self._abort_if_unique_id_mismatch(reason="not_the_same_stick")
                 return self.async_update_reload_and_abort(
                     reconfigure_entry,
-                    data_updates={CONF_USB_PATH: device_path},
+                    data_updates=full_input,
                 )
 
         return self.async_show_form(
             step_id="reconfigure",
-            data_schema=self.add_suggested_values_to_schema(
-                data_schema=vol.Schema(
-                  {vol.Required(CONF_USB_PATH): vol.In(list_of_ports)}
-                ),
-                suggested_values=reconfigure_entry.data,
+            data_schema=vol.Schema(
+                {vol.Required(CONF_USB_PATH): str}  #vol.In(list_of_ports)}
             ),
             description_placeholders={"title": reconfigure_entry.title},
             errors=errors,
