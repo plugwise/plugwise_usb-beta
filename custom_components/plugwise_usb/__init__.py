0f4ec5064c85fba10e7e9cc83a38ec5bcef27029
Bouwe Westerdijk <bouwe.s.westerdijk@gmail.com>
2026-09-05 14:40:54 +0200

    Revert change, improve raise-message instead

--- custom_components/plugwise_usb/__init__.py
+++ custom_components/plugwise_usb/__init__.py
@@ -101,10 +101,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: PlugwiseUSBConfig
     _LOGGER.info("Start to discover the Plugwise network coordinator (Circle+)")
     try:
         await api_stick.discover_coordinator(load=False)
-    except StickError: # as exc:
-        # await api_stick.disconnect()
-        # raise ConfigEntryNotReady("Failed to connect to Circle+") from exc
-        _LOGGER.warning("Failed to connect to Circle+")
+    except StickError as exc:
+        await api_stick.disconnect()
+        raise ConfigEntryNotReady("No connected Plus-device found, pair with one first e.g. via Source") from exc
 
     # Load platforms to allow them to register for node events
     await hass.config_entries.async_forward_entry_setups(
